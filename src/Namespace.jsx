c9e68d3c39b20b442a79fdc52288942ce50a82c0
Zlatko Fedor <zfedor@gmail.com>
2016-01-03 22:45:32 +0100

    added namespace

---
+++ src/Namespace.jsx
@@ -0,0 +1,38 @@
+import { Component, PropTypes, Children } from 'react';
+
+export default class LocaleProvider extends Component {
+  static propTypes = {
+    namespace: PropTypes.string,
+    replace: PropTypes.bool,
+  };
+
+  static childContextTypes = {
+    namespace: PropTypes.object,
+  };
+
+  getNamespace() {
+    const { namespace, replace } = this.props;
+    if (replace || !namespace) {
+      return namespace;
+    }
+
+    if (this.context.namespace) {
+      const parentNamespace = this.context.namespace.getNamespace();
+      if (parentNamespace) {
+        return `${parentNamespace}.${namespace}`;
+      }
+    }
+
+    return namespace;
+  }
+
+  getChildContext() {
+    return {
+      namespace: this,
+    };
+  }
+
+  render() {
+    return Children.only(this.props.children);
+  }
+}
