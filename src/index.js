0cedb3334eab3b9dfdab0fa2eb1390a08918318b
Zlatko Fedor <zfedor@gmail.com>
2016-01-07 18:37:09 +0100

    added locale switch into the index

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import LocaleProvider from './LocaleProvider';
 import Translate from './Translate';
 import Namespace from './Namespace';
 import TranslateHTML from './TranslateHTML';
+import LocaleSwitch from './LocaleSwitch';
 import { Plural, Gender, Mode, Adapters, Caches } from 'translate-maker';
 
-export { LocaleProvider, TranslateHTML, Namespace };
+export { LocaleProvider, TranslateHTML, Namespace, LocaleSwitch };
 export { Plural, Gender, Mode, Adapters, Caches };
 
 export default Translate;
