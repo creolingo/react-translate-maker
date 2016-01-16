ce0347ef0fa46ea4db5eac8f4829ed3bfdbab43b
Zlatko Fedor <zfedor@gmail.com>
2016-01-16 22:13:40 +0100

    deps jsdom

--- utils/tester.js
+++ utils/tester.js
@@ -1,10 +1,12 @@
-import jsdom from 'jsdom';
 import TestUtils from 'react-addons-test-utils';
 import React from 'react';
 
+const jsdom = require('jsdom');
+
 function renderComponent(react, props) {
   global.document = jsdom.jsdom('<!DOCTYPE html><html><body></body></html>');
-  global.window = global.document.parentWindow;
+  global.window = document.defaultView;
+  global.navigator = {userAgent: 'node.js'};
 
   const rendered = TestUtils.renderIntoDocument(React.createElement(react, props));
 
