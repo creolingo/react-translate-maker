077d2b0ed14b0ef3cea8dbbc074824153a60a505
Zlatko Fedor <zfedor@gmail.com>
2016-02-15 14:33:36 +0100

    export translatemaker

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import Translate from './Translate';
 import Namespace from './Namespace';
 import TranslateHTML from './TranslateHTML';
 import LocaleSwitch from './LocaleSwitch';
-import { Plural, Gender, Mode, Adapters, Caches } from 'translate-maker';
+import TranslateMaker, { Plural, Gender, Mode, Adapters, Caches } from 'translate-maker';
 
 export { LocaleProvider, TranslateHTML, Namespace, LocaleSwitch };
-export { Plural, Gender, Mode, Adapters, Caches };
+export { Plural, Gender, Mode, Adapters, Caches, Translate, TranslateMaker };
 
 export default Translate;
