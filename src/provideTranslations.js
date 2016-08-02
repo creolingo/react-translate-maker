15b0a6b0cba948c832f1b25f43dfefc39d2e9f68
Zlatko Fedor <zfedor@gmail.com>
2016-08-02 21:50:02 +0200

    Release 0.2.5

--- src/provideTranslations.js
+++ src/provideTranslations.js
@@ -1,37 +1,9 @@
-import { PropTypes, Component, createElement } from 'react';
-import inherits from 'inherits';
-import hoistNonReactStatics from 'hoist-non-react-statics';
+import { PropTypes } from 'react';
+import createProvider from 'react-provide-props';
 
-function createComponent(CurrentComponent) {
-  function Connector(...args) {
-    Component.apply(this, args);
-  }
-
-  inherits(Connector, Component);
-
-  const componentName = CurrentComponent.displayName || CurrentComponent.name;
-  Connector.displayName = componentName + 'T';
-  Connector.contextTypes = {
-    t: PropTypes.func.isRequired,
-  };
-
-  Connector.prototype.render = function render() {
-    return createElement(CurrentComponent, {
-      ...this.props,
-      t: this.context.t,
-    });
-  };
-
-  hoistNonReactStatics(Connector, CurrentComponent);
-
-  return Connector;
-}
-
-export default function provideTranslate(...args) {
-  // support decorator pattern
-  if (args.length === 0) {
-    return (ComponentToDecorate) => createComponent(ComponentToDecorate);
-  }
-
-  return createComponent.apply(null, args);
-}
+export default createProvider('TrnaslateProvider', (props, context) => ({
+  t: context.t,
+}), {
+}, {
+  t: PropTypes.func.isRequired,
+});
